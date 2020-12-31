86e34542334aec96f8179590ecc4fdcab18bb08a
htarbiyati <h.tarbiyati@gmail.com>
2020-12-31 04:21:04 +0330

    Working

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+float add(float a,float b);
+
+int factorial(int n) {
+	if (n == 1) {
+		return 1;
+	}
+	else {
+		return n * factorial(n - 1);
+	}
+}
+
+int main(){
+	cout << "HelloW!\n"; 
+	cout << factorial(6);
+	cout << "\n\n\n";
+	cout << add(62.1f, 532.1f) << "\n\n";
+	return 0;
+}
